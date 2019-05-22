cd901866fe2fb41456a58e6c60ff6e2b5193fafa
Darren Farrington <darren.farrington@sparkol.com>
2019-05-22 15:00:10 +0100

    Use SSM to parameterise tests

--- tests/steps/init.js
+++ tests/steps/init.js
@@ -1,33 +1,55 @@
-
+const _ = require('lodash')
 const { promisify } = require('util')
 const awscred = require('awscred')
+const { REGION, STAGE } = process.env
+const AWS = require('aws-sdk')
+AWS.config.region = REGION
+const SSM = new AWS.SSM()
 
 let initialized = false
+
+const getParameters = async (keys) => {
+  const prefix = `/workshop-darren/${STAGE}/`
+  const req = {
+    Names: keys.map(key => `${prefix}${key}`)
+  }
+  const resp = await SSM.getParameters(req).promise()
+  return _.reduce(resp.Parameters, function(obj, param) {
+    obj[param.Name.substr(prefix.length)] = param.Value
+    return obj
+   }, {})
+}
 
 const init = async () => {
   if (initialized) {
     return
   }
 
-  process.env.TEST_ROOT = "https://rmmbh0kdzb.execute-api.us-east-2.amazonaws.com/dev"
-  process.env.restaurants_api      = "https://rmmbh0kdzb.execute-api.us-east-2.amazonaws.com/dev/restaurants"
-  process.env.restaurants_table    = "restaurants-darren"
-  process.env.AWS_REGION           = "us-east-2"
+  const params = await getParameters([
+    'table_name', 
+    'cognito_user_pool_id', 
+    'cognito_web_client_id',
+    'cognito_server_client_id',
+    'url'
+  ])
 
-  //process.env.cognito_user_pool_id = "test_cognito_user_pool_id"
-  //process.env.cognito_client_id    = "test_cognito_client_id"
-  process.env.cognito_user_pool_id = "us-east-2_CBLMo4oSE"
-  process.env.cognito_client_id    = "3kt03pov79gef4h8ivuthrg4md"
-  process.env.cognito_server_client_id = "5nkgf5elrtmbvht2i9vuo68v00" 
+  console.log('SSM params loaded')
+
+  process.env.TEST_ROOT                = params.url
+  process.env.restaurants_api          = `${params.url}/restaurants`
+  process.env.restaurants_table        = params.table_name
+  process.env.AWS_REGION               = REGION
+  process.env.cognito_user_pool_id     = params.cognito_user_pool_id
+  process.env.cognito_client_id        = params.cognito_web_client_id
+  process.env.cognito_server_client_id = params.cognito_server_client_id
   
   const { credentials } = await promisify(awscred.load)()
   
   process.env.AWS_ACCESS_KEY_ID     = credentials.accessKeyId
   process.env.AWS_SECRET_ACCESS_KEY = credentials.secretAccessKey
-  console.log('session token', credentials.sessionToken)
+
   if (credentials.sessionToken) {
-    
-    process.env.AWS_SESSION_TOKEN   = credentials.sessionToken
+    process.env.AWS_SESSION_TOKEN = credentials.sessionToken
   }
 
   console.log('AWS credential loaded')
