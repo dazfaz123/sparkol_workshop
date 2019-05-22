8483dc214ce16fe90663eada98ccc8c082a36b77
Darren Farrington <darren.farrington@sparkol.com>
2019-05-22 12:07:36 +0100

    fixed session token env var

--- tests/steps/init.js
+++ tests/steps/init.js
@@ -24,8 +24,10 @@ const init = async () => {
   
   process.env.AWS_ACCESS_KEY_ID     = credentials.accessKeyId
   process.env.AWS_SECRET_ACCESS_KEY = credentials.secretAccessKey
+  console.log('session token', credentials.sessionToken)
   if (credentials.sessionToken) {
-    process.env.AWS_SECURITY_TOKEN    = credentials.sessionToken
+    
+    process.env.AWS_SESSION_TOKEN   = credentials.sessionToken
   }
 
   console.log('AWS credential loaded')
